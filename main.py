e5e8212fed3449465b8f71c5fdb6202794a7ab3d
Nick Kocela <45176718+nak1518@users.noreply.github.com>
2025-11-19 11:46:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@
 import sys
 
 from Dataclasses import Point, Edge, Trap
-from DAG import Node
+from DAG import Type, Node
 
 # Represent trapezoidal map as doubly connected edge list 
-# (contains vertices, edges, and faces as seperate items with pointers to neighbors). 
+# (contains vertices, edges, and faces as separate items with pointers to neighbors). 
 # Also give everything an ID to make it easy to compare them?
 
 def main(argv):
@@ -20,8 +20,7 @@ def main(argv):
     edges = []
     points = []
     dag = None
-    
-    print("here")
+
     
     # Read the input file
     n = 0
@@ -32,8 +31,8 @@ def main(argv):
                  continue
             tokens = line.split()
             if len(tokens) != 4:
-                print("Error reading input. Terminating")
-                return 1
+                break
+            
             x1 = int(tokens[0])
             y1 = int(tokens[1])
             x2 = int(tokens[2])
@@ -51,20 +50,77 @@ def main(argv):
                 edge_bottom = Edge(point_lb, point_rb)
                 edge_left = Edge(point_lb, point_lt)
                 edge_right = Edge(point_rb, point_rt)
-                edge_top = Edge(point_lt, point_lt)
+                edge_top = Edge(point_lt, point_rt)
                 
-                edge_bottom.traps.append(edge_bottom)
-                
-                
+                edge_bottom.traps.append(bounding_box)
+                edge_left.traps.append(bounding_box)
+                edge_right.traps.append(bounding_box)
+                edge_top.traps.append(bounding_box)
                 
                 bounding_box.edge_bottom = edge_bottom
                 bounding_box.edge_left = edge_left
                 bounding_box.edge_right = edge_right
                 bounding_box.edge_top = edge_top
+                
+                trapezoids.append(bounding_box)
+                
+                continue
+                
+            point_l = Point(x1, y1)
+            point_r = Point(x2, y2)
+            edge = Edge(point_l, point_r)
             
-            if x1 == x2:
-                print("Error, vertical line in input. Terminating")
-                return 1
+            point_l.edges.append(edge)
+            point_r.edges.append(edge)
+            edges.append(edge)
+    
+    # Initialize the DAG for locating points inside trapezoids
+    dag = Node(Type.Leaf)
+    dag.trap = bounding_box
+    
+    # Add each edge one at a time
+    # The input should already be randomized so no need to worry about that
+    for new_edge in edges:
+        
+        # Find each of the trapezoids that were intersected, left to right
+        left_endpoint = new_edge.point_l
+        right_endpoint = new_edge.point_r
+        
+        # Get all the leaf nodes of the trapezoids that were intersected
+        intersected_leaf = dag.find(left_endpoint)
+        intersected_traps = [intersected_leaf.trap]
+        
+        # Travel across the trapezoids to the right until you find one that contains the right endpoint
+        while(not intersected_traps[-1].contains(right_endpoint)):
+            this_trap = intersected_traps[-1]
+            new_trap = this_trap.get_neighbor(this_trap.edge_right) # get_neighbor is stubbed
+            intersected_traps.append(new_trap)
+            
+        # For each trapezoid intersected, fix them
+        for i, intersected_trap in enumerate(intersected_traps):
+            if len(intersected_traps) == 1:
+                # Contains both endpoints
+                pass
+            
+            elif i == 0:
+                # Contains left endpoint
+                pass
+            
+            elif i == len(intersected_traps) - 1:
+                # Contains right endpoint
+                pass
+            
+            else:
+                # Contains no endpoints
+                pass
+            
+            
+    # After the trapezoidal map is created, let the user input a query point
+    # DO INPUT STUFF HERE
+    
+    # Print out the path it took through the DAG
+    
+    # Save the entire node structure to a csv file
             
 if __name__ == "__main__":
     main(sys.argv)
