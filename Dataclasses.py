a2a9ce6e050519a90d08b59184602c30f0fdf319
Nick Kocela <45176718+nak1518@users.noreply.github.com>
2025-11-19 11:45:59 -0500

    Update Dataclasses.py

--- Dataclasses.py
+++ Dataclasses.py
@@ -11,7 +11,7 @@ class Point:
         self.y = y
         
         # Points to incident edges
-        edges = []
+        self.edges = []
         
 class Edge:
     next_id = 0
@@ -24,6 +24,28 @@ class Edge:
         
         # Neighboring trapezoids
         self.traps = []
+    
+    # Given a point, determines if it is above or below this segment
+    def isAbove(self, point):
+        if self.point_l.x == self.point_r.x:
+            # Vertical segments break can break things so handle it here
+            if point.y > self.point_l.y and point.y > self.point_r.y:
+                return True
+            else:
+                return False
+        if self.orientation(point) > 0:
+            return True
+        else:
+            return False 
+    
+    # Finds the orientation of the point with respect to the endpoints
+    # >0 means it's above, <0 means it's below
+    # Assumes a is to the left of b
+    def orientation(self, point):
+        a = self.point_l
+        b = self.point_r
+        return (a.x*b.y-b.x*a.y)-(a.x*point.y-point.x*a.y)+(b.x*point.y-point.x*b.y)
+        
         
 class Trap:
     next_id = 0
@@ -31,9 +53,36 @@ class Trap:
         self.id = Trap.next_id
         Trap.next_id += 1
         
-        vertices = []
+        self.vertices = []
         
-        edge_top = None
-        edge_bot = None
-        edge_left = None
-        edge_right = None
+        self.edge_top = None
+        self.edge_bottom = None
+        self.edge_left = None
+        self.edge_right = None
+    
+    # Checks if this trapezoid contains the point
+    # Doesn't count points on the edge (should it?)
+    # Assumes both side edges are vertical (they should always be vertical)
+    def contains(self, point):
+        if self.edge_top.point_l.x > point.x or self.edge_top.point_r.x < point.x:
+            return False
+        elif self.edge_top.orientation(point) > 0 or self.edge_bottom.orientation(point) < 0:
+            return False
+        else:
+            return True
+    
+    # get the neighboring trapezoid based on the edge between them
+    def get_neighbor(self, between_edge):
+        edges = [self.edge_top, self.edge_right, self.edge_left, self.edge_bottom]
+        for edge in edges:
+            # Left or right edges can be none
+            if edge == None:
+                continue
+            if edge.id == between_edge.id:
+                traps = edge.traps
+                for trap in traps:
+                    if trap.id != self.id:
+                        return trap
+        # If no trapezoid was returned, error I guess
+        print("uh oh, trapezoid neighbor returned none")
+        return None
